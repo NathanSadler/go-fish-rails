0ca176156e0432fc6183dcc889d0994473527653
Nathan Sadler <nathan@sadlerville.com>
2021-07-09 15:27:19 -0400

    ???

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,23 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
-    it "returns http success" do
-      get "/sessions/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/sessions/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/sessions/destroy"
-      expect(response).to have_http_status(:success)
+    it "should return new" do
+      get login_path
+      expect(request.path).to(eq("/login"))
     end
   end
 
