034c7e8e3d6d9c5a5be47cd260646bf7f6f26a07
Nathan Sadler <nathan@sadlerville.com>
2021-07-09 12:00:16 -0400

    add tests for user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(name: "Test user", email: "user@example.com")}
+
+  it("requires a name to be present") do
+    user.name = " "
+    expect(user.valid?).to(eq(false))
+  end
+
+  it("should not have a name longer than 50 chars") do
+    user.name = "A" * 51
+    expect(user.valid?).to(eq(false))
+  end
+
+  it("should have an email present") do
+    user.email = "  "
+    expect(user.valid?).to(eq(false))
+  end
+
+  it("should not have an email longer than 255 chars") do
+    user.email = ("a" * 244) + "@example.com"
+    expect(user.valid?).to(eq(false))
+  end
+
+  it("accepts valid email addresses") do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                     first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user.valid?).to(eq(true))
+    end
+  end
+
+  it("rejects invalid email addresses") do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user.valid?).to(eq(false))
+    end
+  end
+
+  it("should have unique email addresses") do
+    duplicate_user = user.dup
+    user.save
+    expect(duplicate_user.valid?).to(eq(false))
+  end
+
 end
