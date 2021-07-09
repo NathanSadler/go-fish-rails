0568abdcd51a230a836019d63174ce4fb21074e8
Nathan Sadler <nathan@sadlerville.com>
2021-07-09 15:08:51 -0400

    add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   get 'signup', to: 'users#new'
   get '/styleguide', to: 'styleguide#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
