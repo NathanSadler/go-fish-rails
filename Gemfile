660324299858a2ff62981391ef6b8f74b426889e
Nathan Sadler <nathan@sadlerville.com>
2021-07-12 09:30:34 -0400

    Fix text on login form submit button

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ group :test do
 end
 gem 'slim'
 gem 'simple_form'
+gem 'simple_form'
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
@@ -7,5 +8,25 @@ RSpec.describe "Sessions", type: :request do
       expect(request.path).to(eq("/login"))
     end
   end
+end
 
+RSpec.describe "Sessions", type: :feature do
+  describe "logging in" do
+    it('directs users to their info page when logging in') do
+      visit login_path
+      binding.pry
+      fill_in '_login_email', with: 'michael@example.com'
+      fill_in '_login_password', with: 'foobar'
+      click_on "Submit"
+    end
+    it('redirects users to the login page if they enter invalid info') do
+      #binding.pry
+      #debugger
+      visit login_path
+      fill_in '_login_email', with: 'bob@example.com'
+      fill_in '_login_password', with: 'c'
+      click_on "Submit"
+      expect(1 == 2).to(eq(true))
+    end
+  end
 end
