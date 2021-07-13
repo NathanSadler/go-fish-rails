8417bc8adb49cc87839077e9ac8261aac0b7b77e
Nathan Sadler <nathan@sadlerville.com>
2021-07-13 09:26:44 -0400

    ???

--- spec/requests/site_layout_specs.rb
+++ spec/requests/site_layout_specs.rb
@@ -6,5 +6,9 @@ RSpec.describe "SiteLayouts", type: :request do
       get root_path
       assert_select "a[href=?]", login_path
     end
+
+    it "displays the settings link if the user isn't logged in" do
+      # Add stuff here later
+    end
   end
 end
