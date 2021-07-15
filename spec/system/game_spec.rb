5b4615d99c2bd2f97e66156e70f866221df748eb
Nathan Sadler <nathan@sadlerville.com>
2021-07-15 16:25:14 -0400

    refactor system/game_spec for DRYness

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -33,62 +33,39 @@ RSpec.describe "Game", type: :system do
 
   let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
 
-  scenario("a logged in user creates a game") do
-    login(session)
-    session.visit new_game_path
-    create_game(session, "Automated Test Game", 2)
-    expect(session.body).to(have_content("Automated Test Game"))
-  end
-end
+  describe("creating a game") do
+    it("lets users create new games") do
+      login(session)
+      session.visit new_game_path
+      create_game(session, "Automated Test Game", 2)
+      expect(session.body).to(have_content("Automated Test Game"))
+    end
 
-RSpec.describe 'Game#new', type: :system do
-  before(:each) do
-    User.create(name:"foobar", email:"foo@bar.com", password:"foobar",
-      password_confirmation:"foobar")
-  end
-
-  after(:each) do
-    User.destroy_all
+    it("takes users to the show page of the game they just created") do
+      login(session)
+      create_game(session)
+      expect(session.current_path).to(eq("/games/#{Game.last.id}"))
+    end
   end
 
-  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
+  describe("joining a game") do
+    it("creates a GameUser object for this game and user if one doesn't " +
+    "already exist") do
+      login(session)
+      create_game(session)
+      session.click_on "Join Game"
+      last_gameuser = GameUser.last
+      expect(last_gameuser.game_id).to(eq(Game.last.id))
+      expect(last_gameuser.user_id).to(eq(User.last.id))
+    end
 
-  it("takes users to the show page of the game they just created") do
-    login(session)
-    create_game(session)
-    expect(session.current_path).to(eq("/games/#{Game.last.id}"))
-  end
-end
-
-RSpec.describe "joining a game", type: :system do
-  before(:each) do
-    User.create(name:"foobar", email:"foo@bar.com", password:"foobar",
-      password_confirmation:"foobar")
-  end
-
-  let(:test_user) {User.last}
-
-  after(:each) do
-    User.destroy_all
-  end
-
-  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
-
-  it("creates a GameUser object for this game and user if one doesn't " +
-  "already exist") do
-    login(session)
-    create_game(session)
-    session.click_on "Join Game"
-    last_gameuser = GameUser.last
-    expect(last_gameuser.game_id).to(eq(Game.last.id))
-    expect(last_gameuser.user_id).to(eq(test_user.id))
+    it("shows a waiting_room page when there aren't enough players "+
+    "in the game") do
+      login(session)
+      create_game(session)
+      session.click_on "Join Game"
+      expect(session.body).to(have_content("Waiting for game to start"))
+    end
   end
 
-  it("shows a waiting_room page when there aren't enough players "+
-  "in the game") do
-    login(session)
-    create_game(session)
-    session.click_on "Join Game"
-    expect(session.body).to(have_content("Waiting for game to start"))
-  end
 end
