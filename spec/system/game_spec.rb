43a0ce669099397685bf76435dd907e507e842c2
Nathan Sadler <nathan@sadlerville.com>
2021-07-15 17:05:53 -0400

    waiting list displays names of user in game

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -15,27 +15,29 @@ def create_game(session, game_name = "Test Game", player_count = 2)
 end
 
 RSpec.describe "Game", type: :system do
+
+  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
+  let(:session2) {Capybara::Session.new(:rack_test, Rails.application)}
+
   before(:each) do
     User.create(name:"foobar", email:"foo@bar.com", password:"foobar",
       password_confirmation:"foobar")
+    login(session)
+    login(session2, "michael@example.com", "password")
   end
 
   after(:each) do
     User.destroy_all
   end
 
-  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
-
   describe("creating a game") do
     it("lets users create new games") do
-      login(session)
       session.visit new_game_path
       create_game(session, "Automated Test Game", 2)
       expect(session.body).to(have_content("Automated Test Game"))
     end
 
     it("takes users to the show page of the game they just created") do
-      login(session)
       create_game(session)
       expect(session.current_path).to(eq("/games/#{Game.last.id}"))
     end
@@ -44,20 +46,28 @@ RSpec.describe "Game", type: :system do
   describe("joining a game") do
     it("creates a GameUser object for this game and user if one doesn't " +
     "already exist") do
-      login(session)
       create_game(session)
       session.click_on "Join Game"
       last_gameuser = GameUser.last
       expect(last_gameuser.game_id).to(eq(Game.last.id))
       expect(last_gameuser.user_id).to(eq(User.last.id))
     end
+  end
 
+  describe("showing a game") do
     it("shows a waiting_room page when there aren't enough players "+
     "in the game") do
-      login(session)
       create_game(session)
       session.click_on "Join Game"
       expect(session.body).to(have_content("Waiting for game to start"))
+    end
+
+    it("lists all players in the game in the waiting room") do
+      create_game(session, "Automatic Test Game", 3)
+      session.click_on "Join Game"
+      session2.visit("/games/#{Game.last.id}")
+      session2.click_on("Join Game")
+      expect(session2.body).to(have_content("foobar"))
     end
   end
 
