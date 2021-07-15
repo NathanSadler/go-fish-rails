fa208f665090bb43f888817bf9086952f3661df8
Nathan Sadler <nathan@sadlerville.com>
2021-07-15 13:56:04 -0400

    fix broken test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'pry'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Users", type: :system do
   describe "GET /new" do
     it "returns the signup page" do
       get signup_path
@@ -10,14 +10,23 @@ RSpec.describe "Users", type: :request do
   end
 end
 
-RSpec.describe "Users", type: :feature do
-  let!(:test_user) {User.create(name: "Test User", email: "michael@example.com",
-    password: "foobar", password_confirmation: "foobar")}
+RSpec.describe "Users", type: :system do
+
+  before(:each) do
+    User.create(name: "Test User", email: "foobar@example.com",
+      password: "foobar", password_confirmation: "foobar")
+  end
+
+  let(:test_user) {User.last}
+
+  after(:each) do
+    User.destroy_all
+  end
 
   describe "logging in" do
     it('directs users to their info page when logging in') do
       visit login_path
-      fill_in 'session_email', with: 'michael@example.com'
+      fill_in 'session_email', with: 'foobar@example.com'
       fill_in 'session_password', with: 'foobar'
       click_on "Submit"
       expect(current_path).to(eq("/users/#{User.last.id}"))
