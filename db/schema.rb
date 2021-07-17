49831e916772fe03999d2538ce56de0d004c61db
Nathan Sadler <nathan@sadlerville.com>
2021-07-17 15:21:01 -0400

    add maximium_player_count to game column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_171445) do
+ActiveRecord::Schema.define(version: 2021_07_17_191813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_171445) do
     t.bigint "winner_id"
     t.string "title"
     t.integer "minimum_player_count"
+    t.integer "maximum_player_count"
   end
 
   create_table "users", force: :cascade do |t|
