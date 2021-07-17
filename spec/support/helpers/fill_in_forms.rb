07de82b28c360010198a04a2c36f30b72db88d7e
Nathan Sadler <nathan@sadlerville.com>
2021-07-17 16:19:16 -0400

    add spec supports

---
+++ spec/support/helpers/fill_in_forms.rb
@@ -0,0 +1,18 @@
+module FillInForms
+  def create_game(session, game_name = "Test Game", minimum_player_count = 2,
+    maximum_player_count = nil)
+    maximum_player_count.nil? ? (max_players = minimum_player_count) : (max_players = maximum_player_count)
+    session.visit new_game_path
+    session.fill_in 'Game Title', with: game_name
+    session.fill_in 'Minimum Number of Players', with: minimum_player_count.to_s
+    session.fill_in 'Maximum Number of Players', with: max_players
+    session.click_on "Submit"
+  end
+
+  def login(session, email="foo@bar.com", password="foobar")
+    session.visit login_path
+    session.fill_in 'Email', with: email
+    session.fill_in 'Password', with: password
+    session.click_on "Submit"
+  end
+end
--- spec/system/go_fish_spec.rb
+++ spec/system/go_fish_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-def login(session, email="foo@bar.com", password="foobar")
-  session.visit login_path
-  session.fill_in 'Email', with: email
-  session.fill_in 'Password', with: password
-  session.click_on "Submit"
-end
-
-def create_game(session, game_name = "Test Game", player_count = 2)
-  session.visit new_game_path
-  session.fill_in 'Game Title', with: game_name
-  session.fill_in 'Minimum Number of Players', with: player_count.to_s
-  session.click_on "Submit"
-end
-
 RSpec.describe "GoFish", type: :system do
 
   let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
