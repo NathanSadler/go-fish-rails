3e2d0d9a5c09f41307a6da5c08fdcbed8f0a628d
Nathan Sadler <nathan@sadlerville.com>
2021-07-19 13:32:35 -0400

    add serialize for Game and add game#take_turn

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,10 @@
 class Game < ApplicationRecord
   has_many :game_users
   has_many :users, through: :game_users
+  serialize :go_fish, GoFish
 
+  def take_turn(player, requested_player:, requested_rank: "H")
+    go_fish.take_turn
+    !save
+  end
 end
