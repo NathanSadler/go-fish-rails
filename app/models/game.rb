0de063caa636cf8a5ac8732e458693b826b6fa33
Nathan Sadler <nathan@sadlerville.com>
2021-07-19 14:34:34 -0400

    fix tests in models/go_fish_spec.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,17 +3,31 @@ class Game < ApplicationRecord
   has_many :users, through: :game_users
   serialize :go_fish, GoFish
 
-  def take_turn(player, requested_player:, requested_rank: "H")
-    go_fish.take_turn
-    !save
-  end
-
   def add_player(player)
     go_fish.add_player(player)
     !save
   end
 
+  def current_player_index
+    go_fish.current_player_index
+  end
+
+  def deck
+    go_fish.deck
+  end
+
   def players
     go_fish.players
   end
+
+  def set_deck(deck)
+    go_fish.deck.send(:set_cards, deck)
+    !save
+  end
+
+  def take_turn(player, requested_player:, requested_rank: "H")
+    go_fish.take_turn
+    !save
+  end
+
 end
