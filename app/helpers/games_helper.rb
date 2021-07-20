74a51a38540dd26a3d220f164ad86fa6544c9d7c
Nathan Sadler <nathan@sadlerville.com>
2021-07-19 21:56:10 -0400

    fix issue with try_to_start

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -19,7 +19,7 @@ module GamesHelper
 
   def try_to_start(game)
     if (GameUser.where(game_id: game.id).length >= game.minimum_player_count)
-      game = Game.find(game.id)
+      # game = Game.find(game.id)
       game.shuffle
       game.deal_cards
       game.started_at = DateTime.now
