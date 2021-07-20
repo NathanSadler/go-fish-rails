11431e5479c67ae517cd08c60cbac43b9430503c
Nathan Sadler <nathan@sadlerville.com>
2021-07-20 14:10:38 -0400

    added note to self

--- app/models/game.rb
+++ app/models/game.rb
@@ -20,6 +20,7 @@ class Game < ApplicationRecord
     go_fish.find_player_with_user_id(user_id)
   end
 
+  # used in waiting_to_take_turn, but only once. 
   def list_cards_of_player_with_user_id(user_id)
     go_fish.list_cards_of_player_with_user_id(user_id)
   end
