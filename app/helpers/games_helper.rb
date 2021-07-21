6962fa00b247036dc4687818a8cb837597d15822
Nathan Sadler <nathan@sadlerville.com>
2021-07-21 09:48:06 -0400

    remove commented-out code

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,7 +2,6 @@ require 'date'
 module GamesHelper
   def try_to_start(game)
     if (GameUser.where(game_id: game.id).length >= game.minimum_player_count)
-      # game = Game.find(game.id)
       game.shuffle
       game.deal_cards
       game.started_at = DateTime.current
