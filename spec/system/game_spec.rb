a435391c590ef8748b9144df02c9a6c5b3d34a9b
Nathan Sadler <nathan@sadlerville.com>
2021-07-26 18:39:11 -0400

    remove binding.pry

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe "Game", type: :system do
       create_game(session)
       session.click_on "Join Game"
       expect(session.body).to(have_content("Waiting for game to start"))
-      binding.pry
       session.click_on("Try To Start Game")
       expect(session.body).to(have_content("Waiting for game to start"))
     end
