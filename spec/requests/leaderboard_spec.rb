e6f22345eca27581334db073e596eeb370e40583
Nathan Sadler <nathan@sadlerville.com>
2021-07-28 11:30:28 -0400

    began adding tests for leaderboard

--- spec/requests/leaderboard_spec.rb
+++ spec/requests/leaderboard_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Leaderboards", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/leaderboard/index"
+      get "/leaderboard"
+      session = Capybara::Session.new(:rack_test, Rails.application)
       expect(response).to have_http_status(:success)
     end
   end
---
+++ spec/system/leaderboard_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe("Leaderboard"), type: system do
+  let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
+
+  before(:each) do
+    ["lalala", "sasasa", "teeteetee"].each {|name| User.create(name: name, email: "#{name}@gmail.com", password: name, password_confirmation: name)}
+    3.times {Game.new([])}
+    Game.all.each do |game|
+      User.each {|user| GameUser.create(game_id: game.id, user_id: user.id)}
+    end
+  end
+
+  describe("ordering users by games won") do
+    it("lists users by number of games won in descending order") do
+      GameUser.where(user_id: User.all[1].id).each {|gameuser| gameuser.update(is_game_winner: true)}
+      
+    end
+  end
+end
