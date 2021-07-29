c4661ec324520dba3e227f9544b35a2c691fc706
Nathan Sadler <nathan@sadlerville.com>
2021-07-29 13:36:10 -0400

    add new test

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,9 +3,9 @@ class LeaderboardController < ApplicationController
     @users = UserStat.all
   end
 
-  def get_users
-    User.select(:name, "count(case when game_users.is_game_winner then 1 end) as won_games",
-  "count(game_users) as played_games").joins(:games).group(
-    :name).order(won_games: :desc)
-  end
+  # def get_users
+  #   User.select(:name, "count(case when game_users.is_game_winner then 1 end) as won_games",
+  # "count(game_users) as played_games").joins(:games).group(
+  #   :name).order(won_games: :desc)
+  # end
 end
--- spec/system/leaderboard_spec.rb
+++ spec/system/leaderboard_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
+require 'date'
 
 RSpec.describe "Leaderboard", type: :system do
   let(:session) {Capybara::Session.new(:rack_test, Rails.application)}
 
   before(:each) do
-    User.first.destroy
+    # User.first.destroy
     ["lalala", "sasasa", "teeteetee"].each {|name| User.create(name: name, email: "#{name}@gmail.com", password: name, password_confirmation: name)}
     3.times {Game.create()}
     Game.all.each do |game|
       User.all.each {|user| GameUser.create(game_id: game.id, user_id: user.id)}
+      game.update(started_at: DateTime.new(2021, 07, 29, 12, 0, 0), finished_at: DateTime.new(2021, 07, 29, 12, 10, 0))
     end
   end
 
@@ -21,6 +23,7 @@ RSpec.describe "Leaderboard", type: :system do
       GameUser.where(user_id: test_user_b.id).each {|gameuser| gameuser.update(is_game_winner: true)}
       Game.create
       [test_user_a, test_user_b].each {|user| GameUser.create(game_id: Game.last.id, user_id: user.id)}
+      Game.last.update(started_at: DateTime.new(2021, 07, 29, 12, 0, 0), finished_at: DateTime.new(2021, 07, 29, 12, 10, 0))
       go_to_leaderboard(session)
     end
 
@@ -37,6 +40,11 @@ RSpec.describe "Leaderboard", type: :system do
     it("lists the number of games the user played") do
       played_games_count = session.first(:css, 'td:nth-child(4)')
       expect(played_games_count.text).to(eq("4"))
+    end
+
+    it("lists the time the user has spend in game with the format days:hours:minutes:seconds") do
+      game_time = session.first(:css, 'td:nth-child(5)')
+      expect(game_time.text).to(eq("0:0:30:0"))
     end
   end
 
