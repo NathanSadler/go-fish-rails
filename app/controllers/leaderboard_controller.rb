bc9c0ef659ab00db10f00881c03228fe1ee01a93
Nathan Sadler <nathan@sadlerville.com>
2021-07-29 10:50:54 -0400

    add some tests

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -4,7 +4,7 @@ class LeaderboardController < ApplicationController
   end
 
   def get_users
-    User.select(:name, "count(game_users) as won_games").joins(:games).where(
-      game_users: {is_game_winner: true}).group(:name)
+    User.select(:name, "count(case when game_users.is_game_winner then 1 end) as won_games").joins(:games).group(
+    :name).order(won_games: :desc)
   end
 end
--- spec/system/leaderboard_spec.rb
+++ spec/system/leaderboard_spec.rb
@@ -16,6 +16,30 @@ RSpec.describe "Leaderboard", type: :system do
   let(:test_user_b) {User.all[1]} # sasasa
   let(:test_user_c) {User.all[2]} # teeteetee
 
+  describe("listing user information") do
+    before(:each) do
+      GameUser.where(user_id: test_user_b.id).each {|gameuser| gameuser.update(is_game_winner: true)}
+      Game.create
+      [test_user_a, test_user_b].each {|user| GameUser.create(game_id: Game.last.id, user_id: user.id)}
+      go_to_leaderboard(session)
+    end
+
+    it("lists the user's name") do
+      user_names = session.all(:css, 'td:nth-child(2)')
+      expect(user_names[0]).to(eq("sasasa"))
+    end
+
+    it("lists the number of games the user won") do
+      won_games_count = session.first(:css, 'td:nth-child(3)')
+      expect(won_games_count).to(eq(2))
+    end
+
+    it("lists the number of games the user played") do
+      played_games_count = session.first(:css, 'td:nth-child(4)')
+      expect(won_games_count).to(eq(3))
+    end
+  end
+
   describe("ordering users") do
     it("lists users by number of games won in descending order") do
       GameUser.where(user_id: test_user_b.id).each {|gameuser| gameuser.update(is_game_winner: true)}
@@ -24,11 +48,12 @@ RSpec.describe "Leaderboard", type: :system do
       expect(user_names[0].text).to(eq(test_user_b.name))
     end
 
-    # it("includes users that haven't won any games") do
-    #   GameUser.where(user_id: test_user_b.id).each {|gameuser| gameuser.update(is_game_winner: true)}
-    #   expect(session).to(have_content("sasasa"))
-    #   expect(session).to(have_content("teeteetee"))
-    # end
+    it("includes users that haven't won any games") do
+      GameUser.where(user_id: test_user_b.id).each {|gameuser| gameuser.update(is_game_winner: true)}
+      go_to_leaderboard(session)
+      expect(session).to(have_content("sasasa"))
+      expect(session).to(have_content("teeteetee"))
+    end
 
   end
 end
