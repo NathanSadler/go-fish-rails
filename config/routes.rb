45822417a15c112818f3626941c12e09b9da8cd0
Nathan Sadler <nathan@sadlerville.com>
2021-07-30 11:03:26 -0400

    add page for game time leaderboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'leaderboards/games_won', to: 'leaderboard#games_won', as: :games_won_leaderboard
   get 'leaderboards/games_played', to: 'leaderboard#games_played', as: :games_played_leaderboard
+  get 'leaderboards/game_time', to: 'leaderboard#game_time', as: :game_time_leaderboard
   get 'go_fish/show'
   get 'go_fish/edit'
   root 'static_pages#home'
