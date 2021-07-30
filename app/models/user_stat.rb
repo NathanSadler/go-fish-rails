d4b2eeee87ee0286563842a45be60b383dcd046c
Nathan Sadler <nathan@sadlerville.com>
2021-07-30 15:04:44 -0400

    move code that formats in game time to user stat controller

--- app/models/user_stat.rb
+++ app/models/user_stat.rb
@@ -4,4 +4,9 @@ class UserStat < ApplicationRecord
   def readonly?
     true
   end
+
+  def formatted_duration
+    in_game_time = game_time.parts
+    "#{in_game_time[:hours].to_i}:#{in_game_time[:minutes].to_i}:#{in_game_time[:seconds].to_i}"
+  end
 end
