d030fa75a2a8811f1f96e4b91460dddfac41ed88
Nathan Sadler <nathan@sadlerville.com>
2021-07-30 15:53:34 -0400

    username can no longer use non-ascii characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :games, through: :game_users
   before_save {self.email = email.downcase}
 
-  
-  validates(:name, {presence: true, length: {maximum:50}})
+  VALID_USERNAME_REGEX = /\A\p{ASCII}+\z/
+  validates(:name, {presence: true, format: {with: VALID_USERNAME_REGEX}, length: {maximum:50}})
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, {presence: true, length: {maximum:255},
