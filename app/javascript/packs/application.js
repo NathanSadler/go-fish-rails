a373663041b5b1c078c7f70875533a6d32463844
Nathan Sadler <nathan@sadlerville.com>
2021-09-16 15:33:33 -0400

    fix some stuff that got overwritten

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "controllers"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
