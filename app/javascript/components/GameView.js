373f07dade910acee4ee5d9faffff47a46ca1d9e
Nathan Sadler <nathan@sadlerville.com>
2021-09-20 09:02:54 -0400

    Fix GameView component

--- app/javascript/components/GameView.js
+++ app/javascript/components/GameView.js
@@ -1,43 +1,55 @@
-import React from "react"
-import PropTypes from "prop-types"
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import Opponent from './Opponent';
 
 class GameView extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = {}
+    this.state = null;
   }
 
-  render () {
-    return (
+  render() {
+    return this.state ? (
       <div>
-       {this.state.cards_in_deck}
+        {this.state.cards_in_deck} <ul>{this.getOpponents()}</ul>
       </div>
+    ) : (
+      <div>Loading...</div>
     );
   }
 
   componentDidMount() {
-    this.fetchGame(this.props.path)
+    this.fetchGame(this.props.path);
   }
 
   async fetchGame(path) {
-   const response = await fetch(path, 
-     {
-       headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json'
-       }
-   }) 
-   
-   const json = await response.json()
-   this.handleServerUpdate(json)
+    // debugger
+    const response = await fetch(path, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const json = await response.json();
+    this.handleServerUpdate(json);
+  }
+
+  getOpponents() {
+    return this.state.opponent_gameuser_ids.map((id) => (
+      <li key={id}>
+        <Opponent gamePlayerPath={`/game_user/${id}`} />
+      </li>
+    ));
   }
 
   handleServerUpdate(json) {
     this.setState({
       cards_in_deck: json['cards_in_deck'],
-    })
+      opponent_gameuser_ids: json['opponent_gameuser_ids'],
+      player: json['user_player']
+    });
   }
 }
 
@@ -45,5 +57,4 @@ GameView.propTypes = {
   path: PropTypes.string.isRequired
 };
 
-
-export default GameView
+export default GameView;
